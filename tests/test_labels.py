ffaa94886ab0191e07a7c34899ff8c2af2b6ff05
AruzhanIbr <ibragimova080499@gmail.com>
2022-10-02 01:08:23 +0600

    add test_labels.py

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -1,17 +1,32 @@
-from selene import by, be
-from selene.support.shared import browser
+from allure_commons.types import Severity
+
+from tests.test_steps import *
 
 
-def test_just_selene():
-    browser.open('https://github.com')
+def test_dynamic_labels():
+    allure.dynamic.tag('web')
+    allure.dynamic.severity(Severity.MINOR)
+    allure.dynamic.suite('web_issue')
+    allure.dynamic.label('owner', 'AruzhanIbr')
+    allure.dynamic.feature('Поиск Issue')
+    allure.dynamic.story('Неавторизованный пользователь может найти Issue')
+    allure.dynamic.link('https://github.com', name='Testing')
+    open_main_page()
+    search_for_repository('AruzhanIbr/qa_quru_python_2_8_hw')
+    go_to_repository('AruzhanIbr/qa_quru_python_2_8_hw')
+    open_issue_tab()
+    should_see_issue_with_name('allure homework')
 
-    browser.element('.header-search-input').click()
-    browser.element('.header-search-input').send_keys('AruzhanIbr/qa_quru_python_2_8_hw')
-    browser.element('.header-search-input').submit()
 
-    browser.element(by.link_text('AruzhanIbr/qa_quru_python_2_8_hw')).click()
-
-    browser.element('#issues-tab').click()
-
-    browser.element(by.partial_text('allure homework')).should(be.visible)
-
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'AruzhanIbr')
+@allure.feature('Поиск Issue')
+@allure.story('Неавторизованный пользователь может найти Issue')
+@allure.link('https://github.com', name='Testing')
+def test_decorator_labels():
+    open_main_page()
+    search_for_repository('AruzhanIbr/qa_quru_python_2_8_hw')
+    go_to_repository('AruzhanIbr/qa_quru_python_2_8_hw')
+    open_issue_tab()
+    should_see_issue_with_name('allure homework')
